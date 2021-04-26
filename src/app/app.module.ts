d7e4c245f61a70d15c9a5d83cf9c8e2df51ddf82
Srajan Naik <Srajan.Naik@innowise.us>
2021-04-26 10:31:50 +0530

    initial implementation with logic for winning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent,
+    
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserModule,    AppRoutingModule,    BrowserAnimationsModule,
+    ToastrModule.forRoot(), // ToastrModule added
+    FontAwesomeModule  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
